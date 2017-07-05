24e02205a73100f96d91cf737cce6eb9d66100e9
Jash <jash15@gmail.com>
2017-07-05 17:55:53 -0500

    Version 04
    Objeto Calculadora

--- codBase/js/app.js
+++ codBase/js/app.js
@@ -1,132 +1,144 @@
 
-var on = document.getElementById('on')
-var signo = document.getElementById('sign')
-var raiz = document.getElementById('raiz')
-var punto = document.getElementById('punto')
-var igual = document.getElementById('igual')
-var mas = document.getElementById('mas')
-var menos = document.getElementById('menos')
-var por = document.getElementById('por')
-var divide = document.getElementById('dividido')
+var Calculador = {
 
+  init: function(){
+    on = document.getElementById('on')
+    signo = document.getElementById('sign')
+    raiz = document.getElementById('raiz')
+    punto = document.getElementById('punto')
+    igual = document.getElementById('igual')
+    mas = document.getElementById('mas')
+    menos = document.getElementById('menos')
+    por = document.getElementById('por')
+    divide = document.getElementById('dividido')
 
-var cero = document.getElementById('0')
-var uno = document.getElementById('1')
-var dos = document.getElementById('2')
-var tres = document.getElementById('3')
-var cuatro = document.getElementById('4')
-var cinco = document.getElementById('5')
-var seis = document.getElementById('6')
-var siete = document.getElementById('7')
-var ocho = document.getElementById('8')
-var nueve = document.getElementById('9')
+    cero = document.getElementById('0')
+    uno = document.getElementById('1')
+    dos = document.getElementById('2')
+    tres = document.getElementById('3')
+    cuatro = document.getElementById('4')
+    cinco = document.getElementById('5')
+    seis = document.getElementById('6')
+    siete = document.getElementById('7')
+    ocho = document.getElementById('8')
+    nueve = document.getElementById('9')
+    display = document.getElementById('display')
+    num1= 0, num2= 0, operador= "0", x= "0"
 
-var display = document.getElementById('display')
+    this.asignarEventosBotones('tecla')
+    this.Numero(cero,0)
+    this.Numero(uno,1)
+    this.Numero(dos,2)
+    this.Numero(tres,3)
+    this.Numero(cuatro,4)
+    this.Numero(cinco,5)
+    this.Numero(seis,6)
+    this.Numero(siete,7)
+    this.Numero(ocho,8)
+    this.Numero(nueve,9)
 
-var num1=0, num2=0;
-var operador = "0";
-var x="0";
+    this.Operador(mas,1)
+    this.Operador(menos,2)
+    this.Operador(por,3)
+    this.Operador(divide,4)
 
-on.onclick = function(){
-  display.innerHTML="0";
-  x="0"
-}
+    this.On()
+    this.Signo()
+    this.Punto()
+    this.Igual()
 
-signo.onclick = function() {
-  if (display.innerHTML=="0" || display.innerHTML==""){display.innerHTML= "0";}
-  else {
-      if(display.innerHTML.indexOf('-')!=-1){display.innerHTML*="-1";}
-      else {display.innerHTML= "-" + display.innerHTML;}
+  },
+
+  asignarEventosBotones: function(selector){
+    var botones = document.getElementsByClassName(selector);
+    for (var i = 0; i < botones.length; i++) {
+      botones[i].onmousedown = this.BotonPresionado;
+      botones[i].onmouseup = this.BotonNormal;
     }
-  }
+  },
 
-punto.onclick = function() {
-  if(x=="0"){
-    if (display.innerHTML=="0" || display.innerHTML==""){display.innerHTML= "0.";}
-    else {
-        if(display.innerHTML.indexOf('.')!=-1){display.innerHTML+= "";}
-        else {display.innerHTML+= ".";}
+  BotonPresionado: function(evento){
+    evento.target.style="padding: 2px";
+  },
+
+  BotonNormal: function(evento){
+    evento.target.style="";
+  },
+
+  Numero: function(num,n){
+    num.onclick = function(){
+      if(x=="0"){
+        if (display.innerHTML=="0"){display.innerHTML= n;
+        } else {
+          if (display.innerHTML.length<8) {display.innerHTML+= n;}
+        }
       }
-  }
-  if(x=="1"){
-       display.innerHTML= "0.";x="0";
-  }
-}
-
-function Numero(num,n) {
-  num.onclick = function() {
-    if(x=="0"){
-      if (display.innerHTML=="0"){display.innerHTML= n;
-      } else {
-        if (display.innerHTML.length<8) {display.innerHTML+= n;}
+      if(x=="1"){
+           display.innerHTML= n;x="0";
       }
     }
-    if(x=="1"){
-         display.innerHTML= n;x="0";
+  },
+
+  Operador: function(op,i){
+    op.onclick = function(){
+      num1 = display.innerHTML
+      display.innerHTML=""
+      operador = i
+      x="0"
     }
-  }
-  num.onmousedown = function () { BotonPresionado(num) }
-  num.onmouseup = function () { BotonNormal(num) }
-}
+  },
 
-function Operador(op,i) {
-  op.onclick = function(){
-    num1 = display.innerHTML
-    display.innerHTML=""
-    operador = i
-    x="0"
-  }
-  op.onmousedown = function () { BotonPresionado(op) }
-  op.onmouseup = function () { BotonNormal(op) }
-}
+  On: function(){
+    on.onclick = function(){
+       display.innerHTML="0"
+       x="0"
+    }
+  },
 
-function Boton(btn){
-  btn.onmousedown = function () { BotonPresionado(btn) }
-  btn.onmouseup = function () { BotonNormal(btn) }
-}
+  Signo: function(){
+    signo.onclick = function(){
+      if (display.innerHTML=="0" || display.innerHTML==""){display.innerHTML= "0";}
+      else {
+        if(display.innerHTML.indexOf('-')!=-1){display.innerHTML*="-1";}
+        else {display.innerHTML= "-" + display.innerHTML;}
+      }
+    }
+  },
 
-Numero(cero,0)
-Numero(uno,1)
-Numero(dos,2)
-Numero(tres,3)
-Numero(cuatro,4)
-Numero(cinco,5)
-Numero(seis,6)
-Numero(siete,7)
-Numero(ocho,8)
-Numero(nueve,9)
+  Punto: function(){
+    punto.onclick = function(){
+      if(x=="0"){
+        if (display.innerHTML=="0" || display.innerHTML==""){display.innerHTML= "0.";}
+        else {
+            if(display.innerHTML.indexOf('.')!=-1){display.innerHTML+= "";}
+            else {display.innerHTML+= ".";}
+          }
+      }
+      if(x=="1"){
+        display.innerHTML= "0.";x="0";
+      }
+    }
+  },
 
-Operador(mas,1)
-Operador(menos,2)
-Operador(por,3)
-Operador(divide,4)
+  Igual: function(){
+    igual.onclick = function(){
+      if(operador=="1" && x=="0"){num2 = display.innerHTML;display.innerHTML = Suma(num1,num2)}
+      if(operador=="1" && x=="1"){num1 = display.innerHTML;display.innerHTML = Suma(num1,num2)}
+      if(operador=="2" && x=="0"){num2 = display.innerHTML;display.innerHTML = Resta(num1,num2)}
+      if(operador=="2" && x=="1"){num1 = display.innerHTML;display.innerHTML = Resta(num1,num2)}
+      if(operador=="3" && x=="0"){num2 = display.innerHTML;display.innerHTML = Multiplica(num1,num2)}
+      if(operador=="3" && x=="1"){num1 = display.innerHTML;display.innerHTML = Multiplica(num1,num2)}
+      if(operador=="4" && x=="0"){num2 = display.innerHTML;display.innerHTML = Divide(num1,num2)}
+      if(operador=="4" && x=="1"){num1 = display.innerHTML;display.innerHTML = Divide(num1,num2)}
+      x="1"
+    }
+  },
 
-Boton(on)
-Boton(signo)
-Boton(raiz)
-Boton(punto)
-Boton(igual)
+};
+
+Calculador.init();
 
 
-igual.onclick = function(){
-  if(operador=="1" && x=="0"){num2 = display.innerHTML;display.innerHTML = Suma(num1,num2)}
-  if(operador=="1" && x=="1"){num1 = display.innerHTML;display.innerHTML = Suma(num1,num2)}
-  if(operador=="2" && x=="0"){num2 = display.innerHTML;display.innerHTML = Resta(num1,num2)}
-  if(operador=="2" && x=="1"){num1 = display.innerHTML;display.innerHTML = Resta(num1,num2)}
-  if(operador=="3" && x=="0"){num2 = display.innerHTML;display.innerHTML = Multiplica(num1,num2)}
-  if(operador=="3" && x=="1"){num1 = display.innerHTML;display.innerHTML = Multiplica(num1,num2)}
-  if(operador=="4" && x=="0"){num2 = display.innerHTML;display.innerHTML = Divide(num1,num2)}
-  if(operador=="4" && x=="1"){num1 = display.innerHTML;display.innerHTML = Divide(num1,num2)}
-  x="1"
-}
-
-function BotonPresionado(elemento){
-  elemento.style="padding: 2px";
-}
-
-function BotonNormal(elemento){
-  elemento.style="";
-}
 
 function Suma(a,b) {
   result = (Number(a)*10 + Number(b)*10)/10
